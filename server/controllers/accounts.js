54f848df01b0badb0370cee925d95984b206bdc9
arterolocojoga <verrissimoartur@gmail.com>
2026-02-11 15:37:36 -0300

    Initial commit

---
+++ server/controllers/accounts.js
@@ -0,0 +1,21 @@
+class Account{
+    index(){//retorna uma lista de todas as informações
+
+    }
+
+    get(){// pega apenas 1 registro
+
+    }
+
+    create(){//cria um registro (referente ao metodo post)
+
+    }
+
+    update(){//atualiza registros (referenciado pelo put/patch)
+
+    }
+
+    delete(){//apaga/inativa um registro
+
+    }
+}
